8ce1e6478b88b9e07076a190891689b51da2cb16
Ramith Hettiarachchi <ramith@cmu.edu>
2025-01-26 11:26:09 -0500

    add `echo` statements to get an idea till which line the script ran before failing

--- build_env.sh
+++ build_env.sh
@@ -19,8 +19,10 @@ echo "Building omtra into the environment '$ENV_NAME'"
 mamba install pytorch=2.4.0 torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
 mamba install -c pyg pytorch-scatter=2.1.2=py311_torch_2.4.0_cu121 pytorch-cluster -y
 mamba install -c dglteam/label/th24_cu121 dgl -y
-mamba install -c conda-forge pytorch-lightning -y
+mamba install -c conda-forge hydra-core pytorch-lightning -y
 mamba install -c conda-forge rdkit=2023.09.4 pystow einops zarr=3 jupyterlab rich matplotlib -y
+echo "✔ Done installing mamba packages"
 
 pip install wandb useful_rdkit_utils py3Dmol --no-input
 pip install -e ./
+echo "✔ Done installing pip packages"
