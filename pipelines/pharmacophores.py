03c3246ce22d4d768d2a0d8259ef1dbd2c610dcd
Riya <ras425@pitt.edu>
2025-02-03 12:37:55 -0500

    pharmacophore and direction vector scripts

---
+++ pipelines/pharmacophores.py
@@ -0,0 +1,87 @@
+import numpy as np
+import rdkit.Chem as Chem
+from scipy.spatial.distance import cdist
+from pharmvec import GetDonorFeatVects, GetAcceptorFeatVects, GetAromaticFeatVects
+import pprint
+
+smarts_patterns = {
+    'Aromatic': ["a1aaaaa1", "a1aaaa1"],
+    'PositiveIon': ['[+,+2,+3,+4]', "[$(C(N)(N)=N)]", "[$(n1cc[nH]c1)]"],
+    'NegativeIon': ['[-,-2,-3,-4]', "C(=O)[O-,OH,OX1]"],
+    'HydrogenAcceptor': [
+        "[#7&!$([nX3])&!$([NX3]-*=[!#6])&!$([NX3]-[a])&!$([NX4])&!$(N=C([C,N])N)]", 
+        "[$([O])&!$([OX2](C)C=O)&!$(*(~a)~a)]"
+    ],
+    'HydrogenDonor': [
+        "[#7!H0&!$(N-[SX4](=O)(=O)[CX4](F)(F)F)]", "[#8!H0&!$([OH][C,S,P]=O)]", "[#16!H0]"
+    ],
+    'Hydrophobic': [
+        "a1aaaaa1", "a1aaaa1", 
+        "[$([CH3X4,CH2X3,CH1X2,F,Cl,Br,I])&!$(**[CH3X4,CH2X3,CH1X2,F,Cl,Br,I])]",
+        "[$(*([CH3X4,CH2X3,CH1X2,F,Cl,Br,I])[CH3X4,CH2X3,CH1X2,F,Cl,Br,I])&!$(*([CH3X4,CH2X3,CH1X2,F,Cl,Br,I])([CH3X4,CH2X3,CH1X2,F,Cl,Br,I])[CH3X4,CH2X3,CH1X2,F,Cl,Br,I])]([CH3X4,CH2X3,CH1X2,F,Cl,Br,I])[CH3X4,CH2X3,CH1X2,F,Cl,Br,I]",
+        "[CH2X4,CH1X3,CH0X2]~[CH3X4,CH2X3,CH1X2,F,Cl,Br,I]",
+        "[$([CH2X4,CH1X3,CH0X2]~[$([!#1]);!$([CH2X4,CH1X3,CH0X2])])]~[CH2X4,CH1X3,CH0X2]~[CH2X4,CH1X3,CH0X2]",
+        "[$([S]~[#6])&!$(S~[!#6])]"
+    ]
+}
+
+def get_smarts_matches(rdmol, smarts_pattern):
+    """Find positions of a SMARTS pattern in molecule."""
+    feature_positions = []
+    atom_positions = []
+    smarts_mol = Chem.MolFromSmarts(smarts_pattern)
+    rd_matches = rdmol.GetSubstructMatches(smarts_mol, uniquify=True)
+    for match in rd_matches:
+        atoms = [np.array(rdmol.GetConformer().GetAtomPosition(idx)) for idx in match]
+        feature_location = np.mean(atoms, axis=0)
+        
+        atom_positions.append(atoms)
+        feature_positions.append(feature_location)
+
+    return rd_matches, atom_positions, feature_positions
+
+# get the vectors for all matches of a smarts pattern
+def get_vectors(mol, feature, atom_idxs, atom_positions, feature_positions):
+    vectors = []
+    for featAtoms, atomsLoc, featLoc in zip(atom_idxs, atom_positions, feature_positions):
+        if feature == 'Aromatic':
+            vectors.append(GetAromaticFeatVects(atomsLoc, featLoc, scale=1.0)[0]) #pick one of the vectors
+        elif feature == 'HydrogenDonor':
+            vectors.append(GetDonorFeatVects(featAtoms, atomsLoc, mol))
+        elif feature == 'HydrogenAcceptor':
+            vectors.append(GetAcceptorFeatVects(featAtoms, atomsLoc, mol))
+        else:
+            vectors.append(np.zeros(3))
+    return vectors
+
+
+def get_pharmacophore_dict(mol):
+    """Extract pharmacophores and direction vectors from RDKit molecule.
+        
+    Returns
+    -------
+    dictionary : {'FeatureName' : {
+                                   'P': [(coord), ... ],
+                                   'V': [(vec), ... ]
+                                   }
+                 }
+    """
+    
+    pharmacophores = {feature: {'P': [], 'V': []} for feature in smarts_patterns}
+
+    for feature, patterns in smarts_patterns.items():
+        for pattern in patterns:
+            atom_idxs, atom_positions, feature_positions = get_smarts_matches(mol, pattern)
+            if feature_positions:
+                vectors = get_vectors(mol, feature, atom_idxs, atom_positions, feature_positions)
+                pharmacophores[feature]['P'].extend(feature_positions)
+                pharmacophores[feature]['V'].extend(vectors)
+
+    return pharmacophores
+
+'''
+supplier = Chem.SDMolSupplier('test.sdf')
+mol = supplier[0]
+dict = get_pharmacophore_dict(mol)
+pprint.pprint(dict)
+'''
---
+++ pipelines/pharmvec.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def GetAromaticFeatVects(atomsLoc, featLoc, scale: float = 1.0):
+    """Compute the direction vector for an aromatic feature."""
+    
+    v1 = atomsLoc[0] - featLoc
+    v2 = atomsLoc[1] - featLoc
+
+    normal = np.cross(v1, v2)
+    normal = normal / np.linalg.norm(normal)
+    normal *= scale
+    
+    return normal
+
+
+def GetDonorFeatVects(featAtoms, atomsLoc, rdmol):
+    atom_idx = featAtoms[0]
+    atom_coords = atomsLoc[0]
+    vectors = []
+    
+    for nbor in rdmol.GetAtomWithIdx(atom_idx).GetNeighbors():
+        print(nbor.GetAtomicNum())
+        if nbor.GetAtomicNum() == 1:  # hydrogen atom
+            nbor_coords = np.array(rdmol.GetConformer().GetAtomPosition(nbor.GetIdx()))
+            vec = nbor_coords - atom_coords
+            vec = vec / np.linalg.norm(vec)
+            vectors.append(vec)
+        
+    return np.mean(vectors)
+
+
+def GetAcceptorFeatVects(featAtoms, atomsLoc, rdmol):
+    atom_idx = featAtoms[0]
+    atom_coords = atomsLoc[0]
+    vectors = []
+    found_vec = False
+
+    # check if any hydrogen neighbor exists
+    for nbor in rdmol.GetAtomWithIdx(atom_idx).GetNeighbors():
+        if nbor.GetAtomicNum() == 1:
+            nbor_coords = np.array(rdmol.GetConformer().GetAtomPosition(nbor.GetIdx()))
+            vec = nbor_coords - atom_coords
+            vec = vec / np.linalg.norm(vec)
+            vectors.append(vec)
+            found_vec = True
+            break
+
+    if not found_vec:
+        # compute average direction of bonds and reverse it
+        ave_bond = np.zeros(3)
+        cnt = 0
+        
+        for nbor in rdmol.GetAtomWithIdx(atom_idx).GetNeighbors():
+            nbor_coords = np.array(rdmol.GetConformer().GetAtomPosition(nbor.GetIdx()))
+            ave_bond += nbor_coords - atom_coords 
+            cnt += 1
+        
+        if cnt > 0:
+            ave_bond /= cnt
+            ave_bond *= -1  # reverse the direction
+            ave_bond = ave_bond / np.linalg.norm(ave_bond)
+            vectors.append(ave_bond)
+
+    return vectors
