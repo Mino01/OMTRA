eaf653e6628e26d91ce3cfdb4300fc46d3fa7c4e
katztyler01 <tjkatz@andrew.cmu.edu>
2025-05-07 16:54:42 -0400

    add rigid docking task

--- omtra/tasks/tasks.py
+++ omtra/tasks/tasks.py
@@ -166,6 +166,20 @@ class FlexibleDocking(Task):
         'type': 'target_dependent_gaussian',
     }
     conditional_paths = dict(**cpc.ligand_conformer, **cpc.protein)
+    
+
+@register_task("rigid_docking")
+class RigidDocking(Task):
+    """Docking a ligand into the protein structure, assuming no knowledge of the protein structure at t=0"""
+
+    groups_fixed = ["ligand_identity", "protein_identity", "protein_structure"]
+    groups_generated = ["ligand_structure"]
+
+    priors = deepcopy(pc.ligand_conformer)
+    priors["npnde_x"] = {
+        "type": "target_dependent_gaussian",
+    }
+    conditional_paths = dict(**cpc.ligand_conformer, **cpc.protein)
 
 @register_task("expapo_conditioned_ligand_docking")
 class ExpApoConditionedLigandDocking(Task):
