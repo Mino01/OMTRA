fixing vectorized masking

omtra/data/distributions/plinder_dists.py
@@ -126,8 +126,14 @@ def sample_n_lig_atoms_plinder(n_prot_atoms: torch.Tensor = None, n_pharms: torc
 
 
         if n_pharms is not None:
+            n_pharms_idxs = torch.searchsorted(supports['n_pharms'], n_pharms)
+            
+            if not torch.all(supports['n_pharms'][n_pharms_idxs] == n_pharms):
+                 raise ValueError("n_pharms must be in the support of the distribution")
+
             # vectorized masking
-            mask = supports['n_pharms'].unsqueeze(0) >= n_pharms.unsqueeze(1)   # (n_samples, n_pharms_support)
+            mask_cols = torch.arange(p.shape[1]).unsqueeze(0) # (1, n_pharms_support)
+            mask = mask_cols >= n_pharms_idxs.unsqueeze(1)  # (n_samples, n_pharms_support)
             mask = mask.unsqueeze(1).expand(-1, p.shape[1], -1)     # expand to match ligand atoms dimension: (n_samples, n_ligand_atoms_support, n_pharms_support)
             
             # marginalize by summing over valid n_pharms indices